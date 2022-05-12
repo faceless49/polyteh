a0e3ecac7d5aa81c7d264f3719dfd379fe2495f9
faceless49 <digital.zeus@mail.ru>
2022-05-12 12:38:24 +0300

    fix(HashRouter): Change router for github

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import './index.css';
 
@@ -9,9 +9,9 @@ import App from 'App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
 );
